e3c61f8643186bc2ffa539d7d951893535ad51cb
Jimmy5811 <jimmy5811@live.com.au>
2019-04-02 08:13:27 +0000

    [James/Mitchell][#1]Add names into team page

--- client/src/js/pages/teamPage/teamPage.spec.js
+++ client/src/js/pages/teamPage/teamPage.spec.js
@@ -14,6 +14,8 @@ describe('TeamPage', () => {
     });
     it('should contain team members names', () => {
       const page = new TeamPage();
+
+      expect(page.render()).toContain("James");
       expect(page.render()).toContain("Mitchell");
     });
   });
